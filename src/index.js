9674effb8f9c10c58c6e42b29e1d4191b750a2fa
Murali Krishna M <murali24011999@gmail.com>
2022-12-29 21:31:31 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ const fs = require('fs/promises')
 const myFileWriter = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	await fs.writeFile(fileName, fileContent);
 }
 
 const myFileReader = async (fileName) => {
+	const res = await fs.readFile(fileName);
+	return res.toString();
 	// write code here
 	// dont chnage function name
 }
@@ -14,11 +17,15 @@ const myFileReader = async (fileName) => {
 const myFileUpdater = async (fileName, fileContent) => {
 	// write code here
 	// dont chnage function name
+	const res = await fs.appendFile(fileName, fileContent);
+	return res;
 }
 
 const myFileDeleter = async (fileName) => {
 	// write code here
 	// dont chnage function name
+	const res = await fs.unlink(fileName);
+	return res;
 }
 
 
